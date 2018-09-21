e461d4ddf11ffb8c43d393a97c2de40fdb096131
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-09-22 00:58:39 +0700

    Homework: 242B Big Segment Time Limit Exceeded solution

---
+++ week-01/day-01/homework/242_big_segment.py
@@ -0,0 +1,23 @@
+n = int(input())
+segments = []
+
+for i in range(n):
+  segment = list(map(int, input().split()))
+  segments.append(segment)
+
+def cover(list1, list2):
+  return list1[0] <= list2[0] and list2[0] <= list2[1] and list2[1] <= list1[1]
+
+def big_segment(n, segments):
+  index = -1
+  for i in range(n):
+    count = 0
+    for j in range(n):
+      if segments[i] == segments[j]: continue
+      if cover(segments[i], segments[j]):
+        count += 1
+    if count == n:
+      index = i + 1
+  print(index)
+
+big_segment(n, segments)
