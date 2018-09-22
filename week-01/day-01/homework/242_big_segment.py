9c479a90d252ac99cbb35380d7b538ba5d248a59
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-09-22 11:45:58 +0700

    Homework: 242B Big Segment - Add description

--- week-01/day-01/homework/242_big_segment.py
+++ week-01/day-01/homework/242_big_segment.py
@@ -1,3 +1,28 @@
+# NOTE: Below is time limit exceeded solution because Codeforces is overloaded
+# and submitted solution is in queue a long time therefore I cannot finalize
+# an accepted solution
+#
+# Get the number of segments to an integer
+# Get all segments to an array
+#
+# Create a helper method called 'cover'
+# This helper method receives 2 segments list1 and list2, then identify whether
+# list1 covers list2 or not
+#
+# Initialize index variable which value is -1
+# Loop from start to end of array of segments with index i
+#   Initialize count variable which value is 0
+#   Loop from start to end of array of segments with index j
+#     If segments[i] equals to segments[j] skip to next iteration
+#     If segments[i] covers segments[j]
+#       Increase count by 1
+#
+#   After loop with j index finishes, if count equals to number of segments minus 1
+#     Store current index plus 1 to initialized index variable at the beginning
+#     Break from the loop with i
+#
+# Print index variable value
+
 n = int(input())
 segments = []
 
