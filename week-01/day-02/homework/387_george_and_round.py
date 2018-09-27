fe3a7463d4d8b02a8f30a1c63b3dd34368586b47
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-09-27 21:35:54 +0700

    Homework: 387 George and Round modified solution

--- week-01/day-02/homework/387_george_and_round.py
+++ week-01/day-02/homework/387_george_and_round.py
@@ -4,12 +4,14 @@ prepared = list(map(int, input().split()))
 
 i = 0
 j = 0
+count = 0 # Number of good prepared problems
 
 while i < n and j < m:
   if prepared[j] >= good[i]:
     i += 1
     j += 1
+    count += 1
   else:
     j += 1
 
-print(n - i)
+print(n - count)
