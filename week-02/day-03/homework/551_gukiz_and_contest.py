ad11aadba19562d063bf5cf0c6eb3be488e44e52
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-09-29 23:10:58 +0700

    Homework: 551 GukiZ and Contest solution 2

--- week-02/day-03/homework/551_gukiz_and_contest.py
+++ week-02/day-03/homework/551_gukiz_and_contest.py
@@ -4,9 +4,38 @@ def inp():
 n = int(input())
 ratings = list(inp())
 
-# Solution 1: Complexity O(n^2)
+ranks = [0] * n
 
-# ranks = [0] * n
+# Solution 2: Complexity O(nlogn)
+class Student:
+  def __init__(self, id = 0, rating = 0):
+    self.id = id
+    self.rating = rating
+
+  def __lt__(self, other):
+    if (self.rating > other.rating) or (self.rating == other.rating and self.id < other.id):
+      return True
+    else:
+      return False
+
+students = []
+
+for i in range(n):
+  students.append(Student(i, ratings[i]))
+
+students.sort()
+
+ranks[students[0].id] = 1
+
+for i in range(1, n):
+  if students[i].rating == students[i - 1].rating:
+    ranks[students[i].id] = ranks[students[i - 1].id]
+  else:
+    ranks[students[i].id] = i + 1
+
+print(' '.join(map(str, ranks)))
+
+# Solution 1: Complexity O(n^2)
 
 # for i in range(n):
 #   rank = 1
