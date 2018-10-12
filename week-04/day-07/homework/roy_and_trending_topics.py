ae85d236095b8385bc24d95afbb9032c3fffb4ae
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-10-12 21:50:32 +0700

    Homework: Roy and Trending Topics sort solution

---
+++ week-04/day-07/homework/roy_and_trending_topics.py
@@ -0,0 +1,31 @@
+# https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/roy-and-trending-topics-1/
+
+# Using sort: O(NlogN)
+
+class Topic:
+  def __init__(self, id = 0, z_score = 0, change = 0):
+    self.id = id
+    self.z_score = z_score
+    self.change = change
+
+  def __lt__(self, other):
+    if (self.change > other.change) or (self.change == other.change and self.id > other.id):
+      return True
+    return False
+
+N = int(input())
+
+topics = []
+
+for _ in range(N):
+  ID, Z, P, L, C, S = map(int, input().split())
+  new_Z = P * 50 + L * 5 + C * 10 + S * 20
+  change = new_Z - Z
+  topic = Topic(ID, new_Z, change)
+  topics.append(topic)
+
+topics.sort()
+
+for i in range(5):
+  topic = topics[i]
+  print(topic.id, topic.z_score)
