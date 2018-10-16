973c1944cbf68edfced45f6c47c43ed346939b90
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-10-16 19:41:10 +0700

    Midterm: Problem B Pangram

---
+++ week-05/day-09/in-class/pangram.py
@@ -0,0 +1,18 @@
+n = int(input())
+s = input()
+
+s = s.lower()
+alphabet = {}
+count = 0
+
+for i in range(n):
+  if alphabet.get(s[i]) == None:
+    alphabet[s[i]] = 1
+    count += 1
+  else:
+    alphabet[s[i]] += 1
+
+if count == 26:
+  print('YES')
+else:
+  print('NO')
