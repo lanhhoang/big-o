b14b1682a193070786b7cded7c2bc940314efe83
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-10-17 22:42:19 +0700

    Midterm: Problem A Printer Queue

---
+++ week-05/day-09/in-class/printer_queue.py
@@ -0,0 +1,34 @@
+# https://www.spoj.com/problems/PQUEUE/
+
+from collections import deque
+from heapq import heappush, heappop
+
+class Job:
+  def __init__(self, id, priority):
+    self.id = id
+    self.priority = priority
+
+t = int(input())
+
+for _ in range(t):
+  n, m = map(int, input().split())
+  priorities = list(map(int, input().split()))
+  q = deque([])
+  pq = []
+  for i in range(n):
+    q.append(Job(i, priorities[i]))
+    heappush(pq, -priorities[i])
+  time = 1
+  while True:
+    first = q[0]
+    if first.priority < -pq[0]:
+      q.popleft()
+      q.append(first)
+    else:
+      if first.id == m:
+        print(time)
+        break
+      else:
+        time += 1
+        q.popleft()
+        heappop(pq)
