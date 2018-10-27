b3bcbad0b6d5af2abf550aa83438eaa6d82d9538
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-10-27 23:05:07 +0700

    Day-11 Homework: Add Big O notation

--- week-06/day-11/homework/10171_meeting_prof_miguel.py
+++ week-06/day-11/homework/10171_meeting_prof_miguel.py
@@ -20,9 +20,9 @@ while True:
   N = int(input())
   if N == 0:
     break
-  yDist = [[INF for _ in range(n)] for _ in range(n)]
-  mDist = [[INF for _ in range(n)] for _ in range(n)]
-  for _ in range(N):
+  yDist = [[INF for _ in range(n)] for _ in range(n)] # O(n)
+  mDist = [[INF for _ in range(n)] for _ in range(n)] # O(n)
+  for _ in range(N): # O(N)
     age_type, direction, start, end, cost = input().split()
     start = alphabet_to_int(start)
     end = alphabet_to_int(end)
@@ -35,26 +35,28 @@ while True:
       mDist[start][end] = cost
       if direction == 'B':
         mDist[end][start] = cost
-  for i in range(n):
+  for i in range(n): # O(n)
     yDist[i][i] = 0
     mDist[i][i] = 0
   u, v = input().split()
   u = alphabet_to_int(u)
   v = alphabet_to_int(v)
 
-  floyd_warshall(yDist)
-  floyd_warshall(mDist)
+  floyd_warshall(yDist) # O(n ** 3)
+  floyd_warshall(mDist) # O(n ** 3)
 
   minDist = INF
   location = []
-  for i in range(n):
+  for i in range(n): # O(n)
     if yDist[u][i] != INF and mDist[v][i] != INF and yDist[u][i] + mDist[v][i] < minDist:
       minDist = yDist[u][i] + mDist[v][i]
 
   if minDist == INF:
     print('You will never meet.')
   else:
-    for i in range(n):
+    for i in range(n): # O(n)
       if yDist[u][i] + mDist[v][i] == minDist:
         location.append(int_to_alphabet(i))
     print(minDist, ' '.join(location))
+
+# Complexity: O(T * n ** 3)
--- week-06/day-11/homework/arbitrag_arbitrage.py
+++ week-06/day-11/homework/arbitrag_arbitrage.py
@@ -16,21 +16,23 @@ while True:
   V = int(input())
   if V == 0:
     break
-  dist = [[0 for _ in range(V)] for _ in range(V)]
+  dist = [[0 for _ in range(V)] for _ in range(V)] # O(V ** 2)
   currencies = {}
-  for i in range(V):
+  for i in range(V): # O(V)
     currency = input().strip()
     currencies[currency] = i
     dist[i][i] = float(1)
   E = int(input())
-  for _ in range(E):
+  for _ in range(E): # O(E)
     inp = input().split()
     c1, r, c2 = currencies[inp[0]], float(inp[1]), currencies[inp[2]]
     dist[c1][c2] = r
-  res = floyd_warshall(V, dist)
+  res = floyd_warshall(V, dist) # O(V ** 3)
   if res == True:
     print('Case ' + str(case) + ': Yes')
   else:
     print('Case ' + str(case) + ': No')
   case += 1
   input()
+
+# Complexity: O(T * V ** 3)
